e65381e2afae8e188d51f89bf2adae0978e98184
chiragsingla014 <chiragsingla014@protonmail.com>
2025-07-19 05:24:56 +0530

    fix minor bug

--- healthcare_backend/healthcare_backend/settings.py
+++ healthcare_backend/healthcare_backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["healthcare-slox.onrender.com", "127.0.0.1", "localhost", "0.0.0.0"]
 
 
 # Application definition
